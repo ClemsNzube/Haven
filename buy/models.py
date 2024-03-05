75fab0ab6fe8e7e65d732678d2fa1542b3d4a081
Clement Nzubechukwu <clemsnzube@gmail.com>
2024-03-05 09:47:19 +0100

    Create model for buy

--- buy/models.py
+++ buy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class BuyLand(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.full_name
