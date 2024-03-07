45aea6be277803e107a65b0aa69d2dee17ff82a6
Clement Nzubechukwu <clemsnzube@gmail.com>
2024-03-07 10:05:32 +0100

    Create Rent Model

--- rent/admin.py
+++ rent/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import RentHouse
 
 # Register your models here.
+
+admin.site.register(RentHouse)
--- rent/models.py
+++ rent/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+
+class RentHouse(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    message = models.TextField()
+    
+    def __str__(self):
+        return self.full_name
