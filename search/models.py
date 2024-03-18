8d70eb4d84b826cee00c623289680a5ec565bfb9
Clement Nzubechukwu <clemsnzube@gmail.com>
2024-03-18 11:01:19 +0100

    Add search

--- search/models.py
+++ search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class SearchCriteria(models.Model):
+    location = models.CharField(max_length=100)
+    min_price = models.DecimalField(max_digits=10, decimal_places=2)
+    max_price = models.DecimalField(max_digits=10, decimal_places=2)
+    house_type = models.CharField(max_length=50, blank=True, null=True)
+    land_plot_type = models.CharField(max_length=50, blank=True, null=True)
+    # Add more fields as needed
--- search/views.py
+++ search/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .models import SearchCriteria
+from .serializers import SearchCriteriaSerializer
+from house.models import House
+from land.models import Land
 
-# Create your views here.
+class SearchAPIView(generics.ListAPIView):
+    serializer_class = SearchCriteriaSerializer
+
+    def get_queryset(self):
+        # Extract search parameters from request
+        location = self.request.query_params.get('location')
+        min_price = self.request.query_params.get('min_price')
+        max_price = self.request.query_params.get('max_price')
+        house_type = self.request.query_params.get('house_type')
+        land_plot_type = self.request.query_params.get('land_plot_type')
+        # Add more parameters as needed
+
+        # Perform filtering based on search parameters
+        houses = House.objects.filter(location__icontains=location, 
+                                       price__gte=min_price, 
+                                       price__lte=max_price,
+                                       house_type=house_type)
+        lands = Land.objects.filter(location__icontains=location, 
+                                    price__gte=min_price, 
+                                    price__lte=max_price,
+                                    plot_type=land_plot_type)
+        
+        # You can further filter based on additional criteria here
+
+        return houses, lands
